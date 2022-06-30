e89f0f789bb24123eeaffd9730bf33659d92489f
licn1212 <108357347+licn1212@users.noreply.github.com>
2022-06-30 10:53:47 +0300

    шршщжж6

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,33 +3,33 @@ let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYyJ
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoYhRs
 
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM81F
 
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVHzC
 
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
+*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqglb
 
-*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
+*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxa
 
-*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k6J2Dwx7F
+*7.-* https://chat.whatsapp.com/EpzuymKm6lG08k62Dwx7F
 
-*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
+*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGp7rQIw
 
-*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
+*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYKwp7
 
-*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
+*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSZLM9
 
-*11.-* https://chat.whatsapp.com/GKdDtQKWP5LCgpbzucz6kt
+*11.-* https://chat.whatsapp.com/GKdDtQKWP5LCgpbzuc6kt
 
-*12.-* https://chat.whatsapp.com/DZA9djOzBn51ZpsCBj6aAg
+*12.-* https://chat.whatsapp.com/DZA9djOzBn51ZpsCBj6Ag
 
-*13.-* https://chat.whatsapp.com/EIczvnvVZtTKRgxEbWtizv
+*13.-* https://chat.whatsapp.com/EIczvnvVZtTKRgxEbWtiv
 
-*14.-* https://chat.whatsapp.com/IR0NQSDFD6UBMT5q30QIFt
+*14.-* https://chat.whatsapp.com/IR0NQSDFD6UBMT5q30QIF
 
 *15.-* https://chat.whatsapp.com/CMd00ZYfwXZHEoEIswHpv3
 `.trim(), wm, media, [['𝙸𝚁 𝙰𝙻 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '.menu']], m)
--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -19,8 +19,8 @@ let text = `
 > pkg install ffmpeg -y
 > pkg install imagemagick -y
 > pkg install yarn
-> git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD
-> cd TheMystic-Bot-MD
+> git clonehttps://github.com/seregr99/Ltaf
+> cd Ltaf
 > yarn install 
 > npm install
 > npm update
@@ -31,7 +31,8 @@ contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: nul
 title: '𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 - 𝙸𝙽𝚂𝚃𝙰𝙻𝙰𝚁𝙱𝙾𝚃',
 body: 'ᴛʜᴇ ᴍʏsᴛᴄ ﹣ ʙᴏᴛ',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
+sourceUrl: `https://github.com/seregr99
+/Ltaf`}}})   
 }
 handler.command = /^(instalarbot)/i
 export default handler
