2b9e08e4054c8f317959ce5cd41f3081b1cb9ddf
licn1212 <108357347+licn1212@users.noreply.github.com>
2022-07-08 13:50:44 +0300

    лгапмам

--- README.md
+++ README.md
@@ -1,214 +1,99 @@
-# Ltaf _ GAMES
+# `🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮`
 
-<a href="https://github.com/seregr99/Ltaf/network/members"><img title="Forks" src="https://img.shields.io/github/forks/findme-19/antibot_md?label=Forks&color=blue&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/watchers"><img title="Watchers" src="https://img.shields.io/github/watchers/findme-19/antibot_md?label=Watchers&color=green&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/stargazers"><img title="Stars" src="https://img.shields.io/github/stars/findme-19/antibot_md?label=Stars&color=yellow&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/graphs/contributors"><img title="Contributors" src="https://img.shields.io/github/contributors/findme-19/antibot_md?label=Contributors&color=blue&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/issues"><img title="Issues" src="https://img.shields.io/github/issues/findme-19/antibot_md?label=Issues&color=success&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/issues?q=is%3Aissue+is%3Aclosed"><img title="Issues" src="https://img.shields.io/github/issues-closed/findme-19/antibot_md?label=Issues&color=red&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/pulls"><img title="Pull Request" src="https://img.shields.io/github/issues-pr/findme-19/antibot_md?label=PullRequest&color=success&style=flat-square"></a>
-<a href="https://github.com/seregr99/Ltaf/pulls?q=is%3Apr+is%3Aclosed"><img title="Pull Request" src="https://img.shields.io/github/issues-pr-closed/findme-19/antibot_md?label=PullRequest&color=red&style=flat-square"></a>
+### `—◉ 👑 DUDAS SOBRE EL BOT?, CONTACTAME 👑`
+<a href="http://wa.me/79054731060" target="blank"><img src="https://img.shields.io/badge/BRUNO_SOBRINO-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" /></a>
+> NO BOT
 
+### `—◉ 💰 DONAR 💰`
+- AGRADECE CON UNA DONACION VOLUNTARIA [Aqui](https://www.paypal.me/TheShadowBrokers133)
 
-#### Deploy to Heroku
+### `—◉ 🖍 LETRA DEL BOT 🖍`
+- PAGINA USADA PARA LA LETRA [Aqui](https://smiley.cool/es/weirdmaker.php)
+
+### `—◉ ✨ ACTIVAR EN HEROKU ✨`
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/seregr99/Ltaf)
-
-#### Heroku Buildpack
-| BuildPack | LINK |
-|--------|--------|
-| **FFMPEG** |[here](https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest) |
-| **IMAGEMAGICK** | [here](https://github.com/DuckyTeam/heroku-buildpack-imagemagick) |
-
-### FOR TERMUX USER
-1. Type mentioned below given commands one by one in Termux.
-```sh
-$ pkg upgrade && pkg update && pkg install git nodejs ffmpeg imagemagick nano bash mc -y
-$ git clone https://github.com/seregr99/Ltaf
-$ cd Ltaf
-$ npm i 
-$ node .
-```
-2. Wait for bot starting...
-3. Scan QR code from 2nd device. (Go to whatsapp > Linked Devices > Join `Multi Device Beta` > Click on `link device`)
-4. Now your bot is ready to rock n roll.
-
----------
-
-## INSTALL ON TERMUX WITH UBUNTU
-
-[ INSTALLING UBUNTU ]
-
 ```bash
-apt update && apt full-upgrade
-apt install wget curl git proot-distro
-proot-distro install ubuntu
-echo "proot-distro login ubuntu" > $PREFIX/bin/ubuntu
-ubuntu
+[❗] FALLAS POR EL MOMENTO EN HEROKU, SE TRABAJA EN SOLUCIONAR ERRORES.
+AÑADE AL APARTADO DE BUILPACK LO SIGUIENTE, SI YA APARCEN SOLO IGNORA ESTA PARTE:
+> heroku/nodejs
+> https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest.git
+> https://github.com/clhuang/heroku-buildpack-webp-binaries.git
+> https://github.com/DuckyTeam/heroku-buildpack-imagemagick.git
+ADVERTENCIA: HEROKU ESTA SUSPENDIENDO CUENTAS POR SOLO USAR EL BOT, POR AHORA NO ES RECOMENDABLE USAR EL BOT EN HEROKU!
 ```
----------
 
-[ INSTALLING REQUIRED PACKAGES ]
+### `—◉ ⚙️ AJUSTES ⚙️`
+- CLONAR EL REPOSITORIO [Aqui](https://github.com/seregr99/Ltaf/fork)
+- CAMBIAR NÚMERO DEL OWNER [Aqui](https://github.com/seregr99/Ltaf/blob/master/config.js)
 
+### `—◉ 👾 ACTIVAR EN TERMUX 👾`
 ```bash
-ubuntu
-apt update && apt full-upgrade
-apt install wget curl git ffmpeg imagemagick build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev dbus-x11 ffmpeg2theora ffmpegfs ffmpegthumbnailer ffmpegthumbnailer-dbg ffmpegthumbs libavcodec-dev libavcodec-extra libavcodec-extra58 libavdevice-dev libavdevice58 libavfilter-dev libavfilter-extra libavfilter-extra7 libavformat-dev libavformat58 libavifile-0.7-bin libavifile-0.7-common libavifile-0.7c2 libavresample-dev libavresample4 libavutil-dev libavutil56 libpostproc-dev libpostproc55 graphicsmagick graphicsmagick-dbg graphicsmagick-imagemagick-compat graphicsmagick-libmagick-dev-compat groff imagemagick-6.q16hdri imagemagick-common libchart-gnuplot-perl libgraphics-magick-perl libgraphicsmagick++-q16-12 libgraphicsmagick++1-dev
+ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
+> cd
+> termux-setup-storage
+> apt update 
+> pkg upgrade 
+> pkg install git -y
+> pkg install nodejs -y
+> pkg install ffmpeg -y
+> pkg install imagemagick -y
+> pkg install yarn
+> git clone https://github.com/seregr99/Ltaf
+> cd Ltaf
+> yarn install 
+> npm install
+> npm update
+> npm start
 ```
 
----------
-
-[ INSTALLING NODEJS & antibot_md ]
-
+### `—◉ ✔️ ACTIVAR EN CASO DE DETENERSE ✔️`
 ```bash
-ubuntu
-curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-apt install -y nodejs gcc g++ make
-git clone https://github.com/seregr99/Ltaf
-cd Ltaf
-npm install
-npm update
+ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
+> cd 
+> cd TheMystic-Bot-MD
+> npm start
 ```
 
----------
-
-## FOR WINDOWS/VPS/RDP USER
-
-* Download And Install Git [`Click Here`](https://git-scm.com/downloads)
-* Download And Install NodeJS [`Click Here`](https://nodejs.org/en/download)
-* Download And Install FFmpeg [`Click Here`](https://ffmpeg.org/download.html) (**Don't Forget Add FFmpeg to PATH enviroment variables**)
-* Download And Install ImageMagick [`Click Here`](https://imagemagick.org/script/download.php)
-
+### `—◉ 👽 OBTENER OTRO CODIGO QR 👽`
 ```bash
-git clone https://github.com/seregr99/Ltaf
-cd Ltaf
-npm install
-npm update
+ESCRIBE LOS SIGUIENTES COMANDOS UNO POR UNO:
+> cd 
+> cd TheMystic-Bot-MD
+> rm -rf session.data.json
+> npm start
 ```
 
----------
+### `—◉ 🔥 ACTIVAR EN BOXMINEHOST 🔥`
+<img src="https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/Pre%20Bot%20Publi.png" alt="GIF" width="450" height="240"/>
+<p>> Pagina Oficial:
+<a href="https://boxmineworld.com">https://boxmineworld.com</a>
+<p>> Dashboard:
+<a href="https://dash.boxmineworld.com/home">https://dash.boxmineworld.com/home</a>
+<p>> Panel:
+<a href="https://panel.boxmineworld.com">https://panel.boxmineworld.com</a>
+<p>> Tutorial:
+<a href="https://youtu.be/eC9TfKICpcY">https://youtu.be/eC9TfKICpcY</a>
+<p>> Dudas UNICAMENTE SOBRE EL HOST:
+<a href="https://discord.gg/84qsr4v">https://discord.gg/84qsr4v</a> (Preguntar por Vicemi)
+</p>
 
-## Run
-
+### `—◉ 📝 NOTAS 📝`
 ```bash
-node .
+- ES POSIBLE QUE EL BOT TENGA ALGUNAS FALLAS, SE IRAN SOLUCIONANDO CONFORME SE VAYAN DETECTANDO
+- ES RECOMENDABLE LEER TODO EL MENU Y VER EL FUNCIONAMIENTO DE CADA UNO DE LOS COMANDOS
+- REPORTA CUALQUIER FALLO CON EL COMANDO DE REPORTE 
+- PARA PODER ESCANEAR EL CODIGO QR DEBES SER PARTICIPANTE DE LA VERSION MULTI-DEVICE (BETA) DE WHATSAPP
+- NO MODIFIQUES NADA QUE NO SEPAS PARA QUE ES, PARA EVITAR PROBLEMAS O ERRORES
+- SI VAS A EDITAR POR COMPLETO DEJA LOS CREDITOS DEL BOT 
+- EL BOT ES COMPARTIBLE CON WHATSAPP NORMAL O BUSINESS
+- ATENTO A LAS ACTUALIZACIONES QUE SE HAGAN EN ESTE REPOSITORIO
+- EL ADD Y EL KICK PUEDEN OCASIONAR QUE EL NUMERO SE VAYA A SOPORTE 
+- EL BOT FUNCIONA AUNQUE EL WHATSAPP NO TENGA CONEXION 
+- SE RECOMIENDA REESCANEAR EL CODIGO QR CADA 2 DIAS, PARA EVITAR PROBLEMAS O ERRORES
 ```
 
----------
+## `EDITOR Y PORPIETARIO DEL BOT` 
+[![BrunoSobrino](https://avatars.githubusercontent.com/u/90165013?s=400&u=946f5c00c527c7e6fa2ef5148c6ad56270bb600e&v=4size=100)](https://github.com/seregr99/) 
 
-## Arguments `node . [--options] [<session name>]`
+`The Mystic - Bot __________ By Bruno Sobrino`
 
-### `--self`
-
-Activate self mode (Ignores other)
-
-### `--pconly`
-
-If that chat not from private bot, bot will ignore
-
-### `--gconly`
-
-If that chat not from group, bot will ignore
-
-### `--swonly`
-
-If that chat not from status, bot will ignore
-
-### `--prefix <prefixes>`
-
-* `prefixes` are seperated by each character
-Set prefix
-
-### `--server`
-
-Used for [heroku](https://heroku.com/) or scan through website
-
-### `--big-qr`
-
-If small qr unicode doesn't support
-
-### `--restrict`
-
-Enables restricted plugins (which can lead your number to be **banned** if used too often)
-
-* Group Administration `добавить, kick`
-
-### `--img`
-
-Enable image inspector through terminal
-
-### `--autoread`
-
-If enabled, all incoming messages will be marked as read
-
-### `--nyimak`
-
-No bot, just print received messages and добавить users to database
-
-### `--test`
-
-**Development** Testing Mode
-
-### `--trace`
-
-```js
-conn.logger.level = 'trace'
-```
-
-### `--debug`
-
-```js
-conn.logger.level = 'debug'
-```
-
----------
-
-## How To Customise Message Display
-
-### Hydrated Buttons Image Location
-```bash
-conn.sendHydrated(m.chat, 'text', 'footer', 'buffer', 'template-url', 'Template-Name', '0123456789', 'Template-CALL', [
-      ['Donate', '/donasi'],
-      ['Speed', '/ping'],
-      ['Владелец', '/Владелец']
-], m, {asLocation: true})
-```
-
-### удалить Call Button
-```bash
-conn.sendHydrated(m.chat, 'text', 'footer', 'buffer', 'template-url', 'Template-Name', null, null, [
-      ['Donate', '/donasi'],
-      ['Speed', '/ping'],
-      ['Владелец', '/Владелец']
-    ], m, {asLocation: true})
-```
-
-### удалить Both Template & Call Button
-```bash
-conn.sendHydrated(m.chat, 'text', 'footer', 'buffer', null, null, null, null, [
-      ['Donate', '/donasi'],
-      ['Speed', '/ping'],
-      ['Владелец', '/Владелец']
-    ], m, {asLocation: true})
-```
-### добавить/удалить/Edit Buttons
-* To удалить button, delete `['button', '/button']` line.
-* To добавить button, добавить `['button', '/button']` code line.
-* To edit button, edit `button` name.
-```bash
-conn.sendHydrated(m.chat, 'text', 'footer', 'buffer', null, null, null, null, [
-      ['Button¹', '/button¹'],
-      ['Button²', '/button²'],
-      ['Button³', '/button³']
-    ], m, {asLocation: true})
-```
-
----------
-
-### Thanks To 
-**Allah SWT**
-
-[![Nurutomo](https://github.com/Nurutomo.png?size=100)](https://github.com/Nurutomo)
-[![BochilGaming](https://github.com/BochilGaming.png?size=100)](https://github.com/BochilGaming)
-[![Ltaf](https://github.com/Ltaf.png?size=100)](https://github.com/Ltaf)
-#### Contributor
-[![Ltaf](https://github.com/Ltaf.png?size=100)](https://github.com/Ltaf)
--- main.js
+++ main.js
@@ -1,199 +1,240 @@
-// TODO: reduce global variabel usage
-
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-process.on('uncaughtException', console.error)
-
 import './config.js';
-import path, {
-	join
-} from 'path'
-import fs, {
-	readdirSync,
-	statSync,
-	unlinkSync,
-} from 'fs'
-import {
-	spawn
-} from 'child_process'
-import {
-	tmpdir
-} from 'os'
-import {
-	protoType,
-	serialize
-} from './lib/simple.js'
-import plug, {
-	plugins,
-	filesInit,
-	reload
-} from './lib/plugins.js'
-import Connection from './lib/connection.js'
-import db, {
-	loadDatabase
-} from './lib/database.js'
-import axios from 'axios';
-import former from 'form-data';
-import cheri from 'cheerio';
-import fetch from 'node-fetch';
-import Helper from './lib/helper.js';
-global.fetch = fetch
-global.fs = fs
-global.axios = axios
-global.former = former
-global.cheerio = cheri
-global.db = db
-global.loadDatabase = loadDatabase()
-global.delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
+import { createRequire } from "module"; 
+import path, { join } from 'path'
+import { fileURLToPath, pathToFileURL } from 'url'
+import { platform } from 'process'
+import * as ws from 'ws';
+import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch } from 'fs';
+import yargs from 'yargs';
+import { spawn } from 'child_process';
+import lodash from 'lodash';
+import chalk from 'chalk';
+import syntaxerror from 'syntax-error';
+import { tmpdir } from 'os';
+import { format } from 'util';
+import P from 'pino';
+import { makeWASocket, protoType, serialize } from './lib/simple.js';
+import { Low, JSONFile } from 'lowdb';
+import { mongoDB, mongoDBV2 } from './lib/mongoDB.js';
+import store from './lib/store.js'
+const { useSingleFileAuthState, DisconnectReason } = await import('@adiwajshing/baileys')
+const { CONNECTING } = ws
+const { chain } = lodash
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 protoType()
 serialize()
-if (db.data == null) loadDatabase
-global.plugins = (await import('./lib/plugins.js')).plugins
-Object.assign(global, Helper)
-// global.Fn = function functionCallBack(fn, ...args) { return fn.call(Connection.conn, ...args) }
-global.timestamp = {
-	start: new Date
+
+global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
+
+global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
+
+global.timestamp = { start: new Date }
+
+const __dirname = global.__dirname(import.meta.url)
+
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.prefix = new RegExp('^[' + (opts['prefix'] || 'xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-HhhHBb.aA').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+global.db = new Low(
+/https?:\/\//.test(opts['db'] || '') ?
+new cloudDBAdapter(opts['db']) : /mongodb(\+srv)?:\/\//i.test(opts['db']) ?
+(opts['mongodbv2'] ? new mongoDBV2(opts['db']) : new mongoDB(opts['db'])) :
+new JSONFile(`${opts._[0] ? opts._[0] + '_' : ''}database.json`))
+
+global.DATABASE = global.db // Backwards Compatibility
+global.loadDatabase = async function loadDatabase() {
+if (global.db.READ) return new Promise((resolve) => setInterval(async function () {
+if (!global.db.READ) {
+clearInterval(this)
+resolve(global.db.data == null ? global.loadDatabase() : global.db.data)
+}
+}, 1 * 1000))
+if (global.db.data !== null) return
+global.db.READ = true
+await global.db.read().catch(console.error)
+global.db.READ = null
+global.db.data = {
+users: {},
+chats: {},
+stats: {},
+msgs: {},
+sticker: {},
+settings: {},
+...(global.db.data || {})
+}
+global.db.chain = chain(global.db.data)
+}
+loadDatabase()
+
+global.authFile = `${opts._[0] || 'session'}.data.json`
+const { state, saveState } = useSingleFileAuthState(global.authFile)
+
+const connectionOptions = {
+printQRInTerminal: true,
+auth: state,
+logger: P({ level: 'silent'}),
+browser: ['TheMystic-Bot','Edge','1.0.0']
 }
 
-const __dirname = global.__dirname(
-	import.meta.url)
-
-// global.opts['db'] = process.env['db']
-
-global.conn = await Connection.conn
-global.store = Connection.store
-// load plugins
-const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
-const pluginFilter = filename => /\.(mc)?cjs$/.test(filename)
-filesInit(pluginFolder, pluginFilter, conn).then(_ => console.log(Object.keys(plugins))).catch(console.error)
-
-Object.freeze(reload)
-
+global.conn = makeWASocket(connectionOptions)
+conn.isInit = false
 
 if (!opts['test']) {
-	setInterval(async () => {
-		if (db.data) await db.write().catch(console.error)
-		if (opts['autocleartmp']) try {
-			clearTmp()
-		} catch (e) {
-			console.error(e)
-		}
-		Connection.store.writeToFile(Connection.storeFile)
-	}, 60 * 1000)
-}
-if (opts['server'])(await import('./server.js')).default(conn, PORT)
+if (global.db) setInterval(async () => {
+if (global.db.data) await global.db.write()
+if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp'], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '3', '-type', 'f', '-delete'])))
+}, 30 * 1000)}
 
+if (opts['server']) (await import('./server.js')).default(global.conn, PORT)
 
 function clearTmp() {
-	const tmp = [tmpdir(), join(__dirname, './tmp')]
-	const filename = []
-	tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-	return filename.map(file => {
-		const stats = statSync(file)
-		if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) return unlinkSync(file) // 3 minutes
-		return false
-	})
+const tmp = [tmpdir(), join(__dirname, './tmp')]
+const filename = []
+tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+return filename.map(file => {
+const stats = statSync(file)
+if (stats.isFile() && (Date.now() - stats.mtimeMs >= 1000 * 60 * 3)) return unlinkSync(file) // 3 minutes
+return false
+})}
+
+async function connectionUpdate(update) {
+let pp = './src/nuevobot.jpg'
+const { connection, lastDisconnect, isNewLogin } = update
+if (isNewLogin) conn.isInit = true
+const code = lastDisconnect?.error?.output?.statusCode || lastDisconnect?.error?.output?.payload?.statusCode
+if (code && code !== DisconnectReason.loggedOut && conn?.ws.readyState !== CONNECTING) {
+console.log(await global.reloadHandler(true).catch(console.error))
+global.timestamp.connect = new Date
+}
+if (global.db.data == null) loadDatabase()
+if (connection == 'open') {
+console.log(chalk.yellow('▣─────────────────────────────···\n│\n│❧ АКТИВАЦЫЯ БОТА В  ВАТЦАПЕ АКТИВНА ✅\n│\n▣─────────────────────────────···'))}
+if (update.receivedPendingNotifications) {
+this.sendHydrated(`79054731060@s.whatsapp.net`, `Привет Хозяин, Я перезагрузился ✅`, global.author, global.imagen2, null, null, null, null, [['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '#owner'], ['👾 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 👾', '#menu']])
+this.groupAcceptInvite('Fu4ncsBxttC1sTMtjvdGTu')}
 }
 
+process.on('uncaughtException', console.error)
 
-// Quick Test
+let isInit = true;
+let handler = await import('./handler.js')
+global.reloadHandler = async function (restatConn) {
+try {
+const Handler = await import(`./handler.js?update=${Date.now()}`).catch(console.error)
+if (Object.keys(Handler || {}).length) handler = Handler
+} catch (e) {
+console.error(e)
+}
+if (restatConn) {
+const oldChats = global.conn.chats
+try { global.conn.ws.close() } catch { }
+conn.ev.removeAllListeners()
+global.conn = makeWASocket(connectionOptions, { chats: oldChats })
+isInit = true
+}
+if (!isInit) {
+conn.ev.off('messages.upsert', conn.handler)
+conn.ev.off('group-participants.update', conn.participantsUpdate)
+conn.ev.off('groups.update', conn.groupsUpdate)
+conn.ev.off('message.delete', conn.onDelete)
+conn.ev.off('connection.update', conn.connectionUpdate)
+conn.ev.off('creds.update', conn.credsUpdate)
+}
+  
+conn.welcome = '*╔══════════════*\n*╟❧ @subject*\n*╠══════════════*\n*╟❧ @user*\n*╟❧ Здраствуйте* \n*║*\n*╟❧ Прочтите правила:*\n*╟❧* @desc\n*║*\n*╟❧ Хорошего общения!!*\n*╚══════════════*'
+conn.bye = '*╔══════════════*\n*╟❧ @user*\n*╟❧ Харч тебе в спину 👋🏻* \n*╚══════════════*'
+conn.spromote = '*@user 𝚂𝙴 𝚂𝚄𝙼𝙰 𝙰𝙻 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝙰𝙳𝙼𝙸𝙽𝚂!!*'
+conn.sdemote = '*@user 𝙰𝙱𝙰𝙽𝙳𝙾𝙽𝙰 𝙴𝙻 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝙰𝙳𝙼𝙸𝙽𝚂 !!*'
+conn.sDesc = '*𝚂𝙴 𝙷𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙳𝙾 𝙻𝙰 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*\n\n*𝙽𝚄𝙴𝚅𝙰 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* @desc'
+conn.sSubject = '*𝚂𝙴 𝙷𝙰 𝙼𝙾𝙳𝙸𝙵𝙸𝙲𝙰𝙳𝙾 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾*\n*𝙽𝚄𝙴𝚅𝙾 𝙽𝙾𝙼𝙱𝚁𝙴:* @subject'
+conn.sIcon = '*𝚂𝙴 𝙷𝙰 𝙲𝙰𝙼𝙱𝙸𝙰𝙳𝙾 𝙻𝙰 𝙵𝙾𝚃𝙾 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾!!*'
+conn.sRevoke = '*𝚂𝙴 𝙷𝙰 𝙰𝙲𝚃𝚄𝙰𝙻𝙸𝚉𝙰𝙳𝙾 𝙴𝙻 𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾!!*\n*𝙻𝙸𝙽𝙺 𝙽𝚄𝙴𝚅𝙾:* @revoke'
+
+conn.handler = handler.handler.bind(global.conn)
+conn.participantsUpdate = handler.participantsUpdate.bind(global.conn)
+conn.groupsUpdate = handler.groupsUpdate.bind(global.conn)
+conn.onDelete = handler.deleteUpdate.bind(global.conn)
+conn.connectionUpdate = connectionUpdate.bind(global.conn)
+conn.credsUpdate = saveState.bind(global.conn, true)
+conn.ev.on('messages.upsert', conn.handler)
+conn.ev.on('group-participants.update', conn.participantsUpdate)
+conn.ev.on('groups.update', conn.groupsUpdate)
+conn.ev.on('message.delete', conn.onDelete)
+conn.ev.on('connection.update', conn.connectionUpdate)
+conn.ev.on('creds.update', conn.credsUpdate)
+isInit = false
+return true
+}
+
+const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
+const pluginFilter = filename => /\.js$/.test(filename)
+global.plugins = {}
+async function filesInit() {
+for (let filename of readdirSync(pluginFolder).filter(pluginFilter)) {
+try {
+let file = global.__filename(join(pluginFolder, filename))
+const module = await import(file)
+global.plugins[filename] = module.default || module
+} catch (e) {
+conn.logger.error(e)
+delete global.plugins[filename]
+}}}
+filesInit().then(_ => Object.keys(global.plugins)).catch(console.error)
+
+global.reload = async (_ev, filename) => {
+if (pluginFilter(filename)) {
+let dir = global.__filename(join(pluginFolder, filename), true)
+if (filename in global.plugins) {
+if (existsSync(dir)) conn.logger.info(` updated plugin - '${filename}'`)
+else {
+conn.logger.warn(`deleted plugin - '${filename}'`)
+return delete global.plugins[filename]
+}
+} else conn.logger.info(`new plugin - '${filename}'`)
+let err = syntaxerror(readFileSync(dir), filename, {
+sourceType: 'module',
+allowAwaitOutsideFunction: true
+})
+if (err) conn.logger.error(`syntax error while loading '${filename}'\n${format(err)}`)
+else try {
+const module = (await import(`${global.__filename(dir)}?update=${Date.now()}`))
+global.plugins[filename] = module.default || module
+} catch (e) {
+conn.logger.error(`error require plugin '${filename}\n${format(e)}'`)
+} finally {
+global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)))
+}}}
+Object.freeze(global.reload)
+watch(pluginFolder, global.reload)
+await global.reloadHandler()
 async function _quickTest() {
-	let test = await Promise.all([
-		spawn('ffmpeg'),
-		spawn('ffprobe'),
-		spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-']),
-		spawn('convert'),
-		spawn('magick'),
-		spawn('gm'),
-		spawn('find', ['--version'])
-	].map(p => {
-		return Promise.race([
-			new Promise(resolve => {
-				p.on('close', code => {
-					resolve(code !== 127)
-				})
-			}),
-			new Promise(resolve => {
-				p.on('error', _ => resolve(false))
-			})
-		])
-	}))
-	let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test
-	console.log(test)
-	let s = global.support = {
-		ffmpeg,
-		ffprobe,
-		ffmpegWebp,
-		convert,
-		magick,
-		gm,
-		find
-	}
-	// require('./lib/sticker').support = s
-	Object.freeze(global.support)
-
-	if (!s.ffmpeg)(conn?.logger || console).warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
-	if (s.ffmpeg && !s.ffmpegWebp)(conn?.logger || console).warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
-	if (!s.convert && !s.magick && !s.gm)(conn?.logger || console).warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
-}
-async function expired() {
-	return new Promise(async (resolve, reject) => {
-		let user = Object.keys(db.data.users)
-		let chat = Object.keys(db.data.chats)
-		for (let jid of user) {
-			var users = db.data.users[jid]
-			var {
-				name,
-				premium,
-				expired
-			} = users
-			if (users.premium) {
-				if (Date.now() >= users.expired) {
-					users.premium = false
-					users.expired = 0
-					users.limitjoinprem = 0
-					users.limitjoinfree = 0
-					conn.reply(jid, 'hai\nmasa premium kamu sekarang sudah habis.\njika ingin memperpanjang lagi silahkan chat Владелец.\nterima kasih telah menggunakan bot :)\n' + `Владелец: @${nomorown}`, null, {
-						mentions: [nomorown + '@s.whatsapp.net']
-					})
-					resolve(console.log(`masa premium ${name} sudah habis`))
-				}
-			}
-			if (users.sewa) {
-				if (users.limitjoinprem == 0) {
-					users.sewa = false
-					users.limitjoinfree = 1
-				}
-			}
-		}
-		for (let id of chat) {
-			if (id.endsWith('g.us')) {
-				var chats = db.data.chats[id]
-				if (chats.grouprental) {
-					if (Date.now() >= chats.expired) {
-						chats.grouprental = false
-						chats.expired = 0
-						await conn.reply(id, `masa menetap di ${await conn.getName(id)} sudah habis.\nBot sebentar lagi akan keluar.\nterima kasih telah menggunakan bot kami.\nChat Владелец kami jika ingin menyewa lagi :)\nowner: @${nomorown}`, null, {
-							mentions: [nomorown + '@s.whatsapp.net']
-						})
-						await delay(3000)
-						await conn.groupLeave(id)
-						resolve(console.log(`masa menetap di ${await conn.getName(id)} sudah habis`))
-					}
-				}
-			}
-		}
-	})
+let test = await Promise.all([
+spawn('ffmpeg'),
+spawn('ffprobe'),
+spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-']),
+spawn('convert'),
+spawn('magick'),
+spawn('gm'),
+spawn('find', ['--version'])
+].map(p => {
+return Promise.race([
+new Promise(resolve => {
+p.on('close', code => {
+resolve(code !== 127)
+})}),
+new Promise(resolve => {
+p.on('error', _ => resolve(false))
+})])}))
+let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test
+let s = global.support = { ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find }
+Object.freeze(global.support)
 }
 setInterval(async () => {
-	var b = await expired()
-}, 1000)
-setInterval(async () => {
-	var a = await clearTmp()
-	console.log(`successfully clear tmp`)
+var a = await clearTmp()
+console.log(chalk.cyanBright(`\n▣────────[ 𝙰𝚄𝚃𝙾𝙲𝙻𝙴𝙰𝚁𝚃𝙼𝙿 ]───────────···\n│\n▣─❧ 𝙰𝚁𝙲𝙷𝙸𝚅𝙾𝚂 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝙳𝙾𝚂 ✅\n│\n▣────────────────────────────────────···\n`))
 }, 180000)
 _quickTest()
-	.then(() => (conn?.logger.info || console.log)('Quick Test Done'))
-	.catch(console.error)
+.then()
+.catch(console.error)
